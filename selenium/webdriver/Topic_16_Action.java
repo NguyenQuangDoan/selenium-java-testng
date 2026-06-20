c729c755a545d1256fe447eb46653c03bc560ee0
Nguyen Quang Doan <quangdoanqwe12@gmail.com>
2026-06-20 11:27:12 +0700

    Add Topic_16_Action with Actions

---
+++ selenium/webdriver/Topic_16_Action.java
@@ -0,0 +1,156 @@
+package webdriver;
+
+import listeners.TestListener;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Listeners(TestListener.class)
+public class Topic_16_Action {
+
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    boolean headless = false;
+    Actions actions;
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Mac")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        }
+
+        FirefoxOptions options = new FirefoxOptions();
+        if (osName.contains("Mac")) {
+            options.setBinary("/Applications/Firefox.app/Contents/MacOS/firefox");
+        }
+        options.addPreference("security.insecure_field_warning.contextual.enabled", false);
+        options.addPreference("security.warn_submit_insecure", false);
+        options.setAcceptInsecureCerts(true);
+        if (headless) {
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        if (!headless) {
+            driver.manage().window().maximize();
+        }
+        actions = new Actions(driver);
+    }
+
+    //@Test
+    public void TC_01_Hover_To_Element() {
+        driver.get("https://automationfc.github.io/jquery-tooltip/");
+        actions.moveToElement(driver.findElement(By.id("age"))).perform();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='ui-tooltip-content']")).getText(), "We ask for your age only for statistical purposes.");
+    }
+    
+    //@Test
+    public void TC_02_Hover_To_Element() throws InterruptedException {
+        driver.get("http://www.myntra.com/");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        
+        // Hover vào Kids menu
+        WebElement kidsMenu = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-group='kids']")));
+        actions.moveToElement(kidsMenu).perform();
+        Thread.sleep(1000);
+        
+        // Đợi và hover vào Home & Bath
+        WebElement homeBathLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='desktop-categoryContainer']//a[text()='Home & Bath']")));
+        actions.moveToElement(homeBathLink).perform();
+        Thread.sleep(500);
+        
+        // Click bằng JavaScript để chắc chắn
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", homeBathLink);
+        Thread.sleep(2000);
+        
+        Assert.assertTrue(driver.getCurrentUrl().contains("kids-home-bath"));
+    }
+
+    //@Test
+    public void TC_03_Hover_To_Element() throws InterruptedException {
+        driver.get("https://www.fahasa.com/");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        
+        // Hover vào Kids menu
+        WebElement iconMenu = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='icon_menu']")));
+        actions.moveToElement(iconMenu).perform();
+        Thread.sleep(1000);
+        
+        // Đợi và hover vào Home & Bath
+        WebElement sachTrongNuoc = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Sách Trong Nước']")));
+        actions.moveToElement(sachTrongNuoc).perform();
+        Thread.sleep(500);
+        
+        // Click bằng JavaScript để chắc chắn
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='fhs_column_stretch']//a[text()='Kỹ Năng Sống']")).isDisplayed());
+    }
+    
+    //@Test
+    public void TC_04_Click_And_Hold_Element() throws InterruptedException {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+        
+        // Click and hold từ item 1 đến item 4
+        WebElement item1 = driver.findElement(By.xpath("//li[text()='1']"));
+        WebElement item4 = driver.findElement(By.xpath("//li[text()='4']"));
+        
+        actions.clickAndHold(item1)
+               .moveToElement(item4)
+               .release()
+               .perform();
+        
+        Thread.sleep(500);
+
+        // Verify các item được chọn (1, 2, 3, 4)
+        List<WebElement> selectedItems = driver.findElements(By.xpath("//li[contains(@class,'ui-selected')]"));
+        Assert.assertEquals(selectedItems.size(), 4);
+        
+        for (int i = 0; i < selectedItems.size(); i++) {
+            Assert.assertEquals(selectedItems.get(i).getText(), String.valueOf(i + 1));   
+        }
+    }
+
+    @Test
+    public void TC_05_Click_And_Select_Random_Element() throws InterruptedException {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+        
+        // Click and hold từ item 1 đến item 4
+        WebElement item1 = driver.findElement(By.xpath("//li[text()='1']"));
+        WebElement item3 = driver.findElement(By.xpath("//li[text()='3']"));
+        WebElement item6 = driver.findElement(By.xpath("//li[text()='6']"));
+        WebElement item11 = driver.findElement(By.xpath("//li[text()='11']"));
+
+        // Verify các item được chọn (1, 2, 3, 4)
+        List<WebElement> itemsToSelect = Arrays.asList(item1, item3, item6, item11);
+        
+        for (WebElement item : itemsToSelect) {
+            actions.click(item).perform();
+            Thread.sleep(200);
+            Assert.assertTrue(item.getAttribute("class").contains("ui-selected"));
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
